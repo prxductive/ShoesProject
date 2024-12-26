aac45939554fc56bfba944412764d3a0e192227e
prxductive <144677265+prxductive@users.noreply.github.com>
2024-12-26 16:16:42 +0300

    Add files via upload

--- gui.py
+++ gui.py
@@ -29,7 +29,7 @@ class AppGUI:
 
         report_button = tk.Button(self.root, text="Отчетность", command=self._open_reports_window)
         report_button.pack(padx=10, pady=10, anchor="ne")
-        report_button.place(relx=0.99, rely=0, anchor="ne")
+        report_button.place(relx=0.98, rely=0.06, anchor="ne")
 
 
     def _create_stock_tab(self):
@@ -48,8 +48,8 @@ class AppGUI:
         tk.Button(stock_tab, text="Новая запись", command=self._open_add_stock_window).pack(pady=10, anchor="se", padx=20, side="right")
 
         self.stock_tree.bind("<Delete>", lambda event: self._delete_record(event, self.stock_tree, "products", "product_id"))
+        self.stock_tree.bind("<Double-1>", lambda event: self._edit_record(event, self.stock_tree, "products", "product_id"))
         self._update_stock_table()
-
 
     def _create_orders_tab(self):
         orders_tab = ttk.Frame(self.notebook)
@@ -65,6 +65,7 @@ class AppGUI:
         self.orders_tree.pack(expand=True, fill='both', padx=20, pady=20)
         tk.Button(orders_tab, text="Новая запись", command=self._open_add_order_window).pack(pady=10, anchor="se", padx=20, side="right")
         self.orders_tree.bind("<Delete>", lambda event: self._delete_record(event, self.orders_tree, "orders", "order_id"))
+        self.orders_tree.bind("<Double-1>", lambda event: self._edit_record(event, self.orders_tree, "orders", "order_id"))
         self._update_orders_table()
 
 
@@ -82,15 +83,31 @@ class AppGUI:
         self.sales_tree.pack(expand=True, fill='both', padx=20, pady=20)
         tk.Button(sales_tab, text="Новая запись", command=self._open_add_sale_window).pack(pady=10, anchor="se", padx=20, side="right")
         self.sales_tree.bind("<Delete>", lambda event: self._delete_record(event, self.sales_tree, "sales", "sale_id"))
+        self.sales_tree.bind("<Double-1>", lambda event: self._edit_record(event, self.sales_tree, "sales", "sale_id"))
         self._update_sales_table()
 
     def _open_reports_window(self):
-        if self.report_window is None or not tk.Toplevel.winfo_exists(self.report_window):
-            self.report_window = tk.Toplevel(self.root)
-            self.report_window.title("Отчетность")
-            self.report_window.state('zoomed')
-            tk.Label(self.report_window, text="Здесь будет отчет", font=("Arial", 12)).pack(padx=10, pady=10)
+         if self.report_window is None or not tk.Toplevel.winfo_exists(self.report_window):
+             self.report_window = tk.Toplevel(self.root)
+             reports_window = self.report_window
+             reports_window.title("Отчетность")
+             reports_window.state('zoomed')
+             tk.Label(reports_window, text="ОТЧЕТ О ВЫРУЧКЕ КОМПАНИИ", font=("Arial", 16)).pack(padx=10, pady=10)
 
+             report_frame = ttk.Frame(reports_window)
+             report_frame.pack(expand=True, fill='both', padx=20, pady=20)
+
+             columns = ("Доходы", "Сумма, руб.")
+             report_tree = ttk.Treeview(report_frame, columns=columns, show='headings')
+
+             for col in columns:
+               report_tree.heading(col, text=col)
+               report_tree.column(col, width=200, anchor='center') # Выравнивание текста в столбце
+
+             report_tree.pack(expand=True, fill='both', padx=10, pady=10)
+             total_revenue = self._update_report_table(report_tree)
+             tk.Label(report_frame, text=f"Выручка компании составляет {total_revenue} руб.", font=("Arial", 12)).pack(padx=10, pady=10, anchor="w")
+             tk.Button(report_frame, text="Назад к производству", command=reports_window.destroy).pack(padx=10, pady=10, anchor="se", side="right")
     def _open_add_stock_window(self):
         if self.add_stock_window is None or not tk.Toplevel.winfo_exists(self.add_stock_window):
             self.add_stock_window = tk.Toplevel(self.root)
@@ -145,7 +162,7 @@ class AppGUI:
             add_order_window = self.add_order_window
             add_order_window.title("Новая запись в заказы")
 
-             # Центрируем окно
+            # Центрируем окно
             add_order_window.update_idletasks()
             width = add_order_window.winfo_width()
             height = add_order_window.winfo_height()
@@ -208,7 +225,7 @@ class AppGUI:
             total_amount_entry = tk.Entry(add_sale_window, font=("Arial", 12))
             total_amount_entry.grid(row=4, column=1, sticky="e", padx=10, pady=5)
             error_label = tk.Label(add_sale_window, text="", font=("Arial", 12), fg="red")
-            error_label.grid(row=5, column=0, columnspan=2, padx=10, pady=5)
+            error_label.grid(row=6, column=0, columnspan=2, padx=10, pady=5)
 
             tk.Button(add_sale_window, text="Записать и закрыть", command= lambda: self._add_sale(
                 order_id_entry.get(),
@@ -219,17 +236,21 @@ class AppGUI:
                 error_label), font=("Arial", 12)).grid(row=5, column=0, columnspan=2, pady=10, sticky="ew", padx=10)
             add_sale_window.focus()
     def _add_stock(self, product_name, material, shoe_type, color, quantity, price, window, error_label):
+        query = "ALTER TABLE products AUTO_INCREMENT = 1"
+        self.db_manager.execute_query(query)
         query = "INSERT INTO products ( product_name, material, shoe_type, color, quantity, price) VALUES (%s, %s, %s, %s, %s, %s)"
         params = (product_name, material, shoe_type, color, quantity, price)
         if self.db_manager.execute_query(query, params):
-          window.destroy()
-          self._update_stock_table()
+            window.destroy()
+            self._update_stock_table()
         else:
             error_label.config(text="Ошибка при добавлении записи")
     def _add_order(self, customer_name, product_name, quantity, status, window, error_label):
         if product_name.lower() not in [row.lower() for row in self._get_products_from_stock()]:
-             error_label.config(text="Такого продукта нет на складе!", wraplength=400)
-             return
+            error_label.config(text="Такого продукта нет на складе!", wraplength=400)
+            return
+        query = "ALTER TABLE orders AUTO_INCREMENT = 1"
+        self.db_manager.execute_query(query)
         query = "INSERT INTO orders (customer_name, product_name, quantity, order_status) VALUES (%s, %s, %s, %s)"
         params = (customer_name, product_name, quantity, status)
         if self.db_manager.execute_query(query, params):
@@ -237,15 +258,21 @@ class AppGUI:
             self._update_orders_table()
         else:
             error_label.config(text="Ошибка при добавлении записи")
+
     def _add_sale(self, order_id, product_name, quantity, total_amount, window, error_label):
         if product_name.lower() not in [row.lower() for row in self._get_products_from_stock()]:
-              error_label.config(text="Такого продукта нет на складе!", wraplength=400)
-              return
+           error_label.config(text="Такого продукта нет на складе!", wraplength=400)
+           return
+        if not self._is_order_exist(order_id):
+           error_label.config(text="Такого заказа не существует", wraplength=400)
+           return
+        query = "ALTER TABLE sales AUTO_INCREMENT = 1"
+        self.db_manager.execute_query(query)
         query = "INSERT INTO sales (order_id, product_name, quantity, total_amount) VALUES (%s, %s, %s, %s)"
         params = (order_id, product_name, quantity, total_amount)
         if self.db_manager.execute_query(query, params):
-             window.destroy()
-             self._update_sales_table()
+           window.destroy()
+           self._update_sales_table()
         else:
              error_label.config(text="Ошибка при добавлении записи")
     def _delete_record(self, event, tree, table, id_col):
@@ -253,21 +280,21 @@ class AppGUI:
         if selected_item:
             record_id = tree.item(selected_item, 'values')[0]
             try:
-                 if table == "products":
+                if table == "products":
                     if self._is_product_used_in_orders(record_id) or self._is_product_used_in_sales(record_id):
                         tk.messagebox.showerror("Ошибка", "Невозможно удалить продукт, так как он используется в заказах или продажах")
                         return
-                 elif table == "orders":
-                     if self._is_order_used_in_sales(record_id):
-                         tk.messagebox.showerror("Ошибка", "Невозможно удалить заказ, так как он используется в продажах")
-                         return
-                 query = f"DELETE FROM {table} WHERE {id_col} = %s"
-                 if not self.db_manager.execute_query(query, (record_id,)):
-                    tk.messagebox.showerror("Ошибка", f"Не удалось удалить запись c ID {record_id}")
-                 else:
-                     tree.delete(selected_item)
+                elif table == "orders":
+                    if self._is_order_used_in_sales(record_id):
+                        tk.messagebox.showerror("Ошибка", "Невозможно удалить заказ, так как он используется в продажах")
+                        return
+                query = f"DELETE FROM {table} WHERE {id_col} = %s"
+                if not self.db_manager.execute_query(query, (record_id,)):
+                   tk.messagebox.showerror("Ошибка", f"Не удалось удалить запись c ID {record_id}")
+                else:
+                   tree.delete(selected_item)
             except Exception as e:
-              tk.messagebox.showerror("Ошибка", f"Невозможно удалить запись, так как она связана с другими таблицами")
+                 tk.messagebox.showerror("Ошибка", f"Невозможно удалить запись, так как она связана с другими таблицами")
     def _is_product_used_in_orders(self, product_id):
         query = "SELECT COUNT(*) FROM orders WHERE product_name = (SELECT product_name FROM products WHERE product_id = %s)"
         params = (product_id,)
@@ -283,13 +310,37 @@ class AppGUI:
         params = (order_id,)
         result = self.db_manager.fetch_all(query, params)
         return result and result[0][0] > 0
+    def _is_order_exist(self, order_id):
+         query = "SELECT COUNT(*) FROM orders WHERE order_id = %s"
+         params = (order_id,)
+         result = self.db_manager.fetch_all(query, params)
+         return result and result[0][0] > 0
     def _get_products_from_stock(self):
         query = "SELECT product_name FROM products"
         rows = self.db_manager.fetch_all(query)
         if rows:
-          return [row[0] for row in rows]
+            return [row[0] for row in rows]
         return []
 
+    def _reset_auto_increment(self, table):
+          query = f"ALTER TABLE {table} AUTO_INCREMENT = 1"
+          self.db_manager.execute_query(query)
+    def _update_report_table(self, tree):
+        for row in tree.get_children():
+            tree.delete(row)
+        query = """
+          SELECT
+          s.product_name,
+          SUM(s.total_amount)
+          FROM sales s
+           GROUP BY s.product_name
+            """
+        rows = self.db_manager.fetch_all(query)
+        total_revenue = 0
+        for row in rows:
+          tree.insert('', 'end', values=(row[0], row[1]))
+          total_revenue += row[1]
+        return total_revenue
     def _update_stock_table(self):
         for row in self.stock_tree.get_children():
             self.stock_tree.delete(row)
